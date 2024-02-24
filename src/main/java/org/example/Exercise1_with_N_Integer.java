5b9a9a525cbe55c1f85883f1ec81f68aa6449f7e
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-24 18:02:19 -0500

    write a method for the sum of integers inside an array  and one for the product of an array

--- src/main/java/org/example/Exercise1_with_N_Integer.java
+++ src/main/java/org/example/Exercise1_with_N_Integer.java
@@ -22,10 +22,26 @@ public class Exercise1_with_N_Integer {
         int[]  integers = new int[n];
 
         for(int i = 0; i < n; i++){
-            System.out.println("Enter your " + n + "num: ");
+            System.out.println("Enter your " + i + 1 + " num: ");
             integers[i] = sc.nextInt();
         }
 
         System.out.println(Arrays.toString(integers));
+    }
+
+    public static int sumOfArray(int[] array){
+        int sum = 0;
+        for(int i = 0; i < array.length; i ++){
+            sum += array[i];
+        }
+        return sum;
+    }
+    public static int produtOfArray(int[] array){
+        int product  = 1;
+        for(int i = 0; i < array.length; i++){
+
+            product *= array[i];
+        }
+        return product;
     }
 }
