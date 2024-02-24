35fbb993b02b482c742aed310ca23ef6210a7bf6
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-24 18:58:42 -0500

    Exercise 1: right a program that print each row sums
    Note that: array.length method returns the number of row
    whereas array[i].length returns the number of element inside of that array the size of that element it self so it does really mattet to say at index i since we assume dthat its not a jagged array

--- src/main/java/org/example/Array2d.java
+++ src/main/java/org/example/Array2d.java
@@ -3,5 +3,24 @@ package org.example;
 public class Array2d {
     public static void main(String[] args) {
 
+        int[][] myArray = {{1,2},
+                {2,3},
+                {3,4}
+        };
+
+        sumOfRow(myArray);
+    }
+
+    public static void sumOfRow(int[][] array){
+        int sum = 0;
+        // Assuming each array have the same number of elements
+        // so not ragged array
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                sum += array[i][j];
+            }
+            System.out.println("Sum of row " + i + " : " + sum);
+            sum = 0;
+        }
     }
 }
