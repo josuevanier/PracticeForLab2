e260ba00eec1522a79f3b4a30addfb9c870d64b0
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-24 19:12:32 -0500

    Max of an Array note that if you want the maximum vlue it cannot be equal to zero since an element of that array can be zero if you want the max you have to set max to such a small value that any other value is bigger than that that was my mistake i set it to integer .max so if you want the max value set it to the minimum if you want the minimum value set it to the maximum

--- src/main/java/org/example/Array2d.java
+++ src/main/java/org/example/Array2d.java
@@ -3,12 +3,14 @@ package org.example;
 public class Array2d {
     public static void main(String[] args) {
 
-        int[][] myArray = {{1,2},
-                {2,3},
-                {3,4}
+        int[][] myArray = {{1,2,3,4},
+                {2,3,5,6,7,8},
+                {3,4,7,8,8}
         };
 
         sumOfRow(myArray);
+
+        maxOfAnArray(myArray);
     }
 
     public static void sumOfRow(int[][] array){
@@ -21,6 +23,17 @@ public class Array2d {
             }
             System.out.println("Sum of row " + i + " : " + sum);
             sum = 0;
+        }
+    }
+    public static void maxOfAnArray(int[][] array){
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                max = Math.max(max,array[i][j]);
+            }
+            System.out.println("Max of " + i + " : " + max);
+            max = Integer.MIN_VALUE;
         }
     }
 }
