8a223d0ebc89fb90a62567d481bd35b9b17b83a7
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-25 20:53:11 -0500

    Practice with Strings is done so compareTO is just a way to have a customize sorting the way you want: impliment the interface Comparable<Author>

--- src/main/java/org/example/Comparator.java
+++ src/main/java/org/example/Comparator.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Comparator {
 
+    public static void main(String[] args) {
+        ArrayList<Author> authors = new ArrayList<Author>();
+
+        authors.add(new Author("Josue", "FOrestal","LOla"));
+        authors.add(new Author("Josue", "BOrestal","LOla"));
+        authors.add(new Author("Josue", "DOrestal","LOla"));
+
+        Collections.sort(authors);
+
+        // Cannot print the values as an object if you don't have a toString method
+        for(Author author : authors){
+            System.out.println( author.firstName + " " + author.lastName + " " + author.bookName);
+        }
+    }
 }
 class Author implements  Comparable<Author>{
     String firstName;
@@ -13,7 +30,8 @@ class Author implements  Comparable<Author>{
         this.lastName = lastName;
         this.bookName = bookName;
     }
-
+    // Problem with comparator one of the major problem with comperator  its the comperator interface only have one method
+    // if you want to compare based  sort based on different data field you will have to change the code
     @Override
     public int compareTo(Author o) {
         return this.lastName.compareTo(o.lastName); // comparing the current object with the object o;
