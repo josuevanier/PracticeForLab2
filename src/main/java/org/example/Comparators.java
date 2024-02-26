282b0abca31488d5a99ed9b6bc8af7f966564431
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-25 21:26:17 -0500

    To use the comparator and not comparable we create a class that has the name of which data field we want to sort or array (based on these values) we sort the array same logic but the class you create impliments Comparator which you need to use java.util Comaprator

--- src/main/java/org/example/Comparators.java
+++ src/main/java/org/example/Comparators.java
@@ -2,17 +2,19 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
-public class Comparator {
+public class Comparators {
 
     public static void main(String[] args) {
         ArrayList<Author> authors = new ArrayList<Author>();
 
-        authors.add(new Author("Josue", "FOrestal","LOla",4));
-        authors.add(new Author("Josue", "BOrestal","LOla",2));
-        authors.add(new Author("Josue", "DOrestal","LOla",3));
+        authors.add(new Author("Josue", "FOrestal","LOla",4,1977));
+        authors.add(new Author("Josue", "BOrestal","LOla",2,2002));
+        authors.add(new Author("Josue", "DOrestal","LOla",3,2004));
 
-        Collections.sort(authors);
+        // Based on year of Publication
+        Collections.sort(authors, new AuthorYearOfPublicationComparator());
 
         // Cannot print the values as an object if you don't have a toString method
         for(Author author : authors){
@@ -24,13 +26,15 @@ class Author implements  Comparable<Author>{
     String firstName;
     String lastName;
     String bookName;
-    int numberOfSells = 0;
+    int numberOfSells;
+    int yearOfPulbication;
 
-    public Author(String firstName, String lastName, String bookName, int numberOfSells) {
+    public Author(String firstName, String lastName, String bookName, int numberOfSells,int yearOfPulbication) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.bookName = bookName;
         this.numberOfSells = numberOfSells;
+        this.yearOfPulbication  = yearOfPulbication;
     }
     // Problem with comparator one of the major problem with comperator  its the comperator interface only have one method
     // if you want to compare based  sort based on different data field you will have to change the code
@@ -40,5 +44,22 @@ class Author implements  Comparable<Author>{
         if(this.numberOfSells == o.numberOfSells) return 0;
         else if (this.numberOfSells > o.numberOfSells) return -1;
         else return 1;
+    }
+}
+class AuthorNameComparator implements Comparator<Author>{
+
+    @Override
+    public int compare(Author o1, Author o2) {
+        return o1.lastName.compareTo(o2.lastName);
+    }
+}
+
+class AuthorYearOfPublicationComparator implements Comparator<Author>{
+
+    @Override
+    public int compare(Author o1, Author o2) {
+        if(o1.yearOfPulbication == o2.yearOfPulbication) return 0;
+        else if (o1.yearOfPulbication > o2.yearOfPulbication) return 1;
+        else return -1;
     }
 }
